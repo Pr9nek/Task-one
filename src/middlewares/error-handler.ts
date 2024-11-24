464d1ba25eefb42128f011c22b1160ba5c2034da
Alex <pr9nek@mail.ru>
2024-11-24 17:20:26 +0400

    feat: error-handler

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+
+interface IError extends Error {
+    statusCode?: number;
+}
+
+const errorHandler = (err: IError, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack); // Логируем ошибку в консоль
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'На сервере произошла ошибка';
+
+    res.status(statusCode)
+        .send({
+        message: message,
+    });
+};
+
+export default errorHandler;
